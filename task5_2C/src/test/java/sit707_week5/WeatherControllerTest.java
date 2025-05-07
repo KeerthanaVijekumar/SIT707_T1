6e42a8ac06888f0d862737e2ebf7b9dd0fe7abcd
KeerthanaVijekumar <s224719679@deakin.edu.au>
2025-05-08 07:02:26 +1000

    Task5.2C Project import

---
+++ task5_2C/src/test/java/sit707_week5/WeatherControllerTest.java
@@ -0,0 +1,118 @@
+package sit707_week5;
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class WeatherControllerTest {
+
+	@Test
+	public void testStudentIdentity() {
+		String studentId = "s224719679";
+		Assert.assertNotNull("Student ID is null", studentId);
+	}
+
+	@Test
+	public void testStudentName() {
+		String studentName = "Keerthana Vijekumar";
+		Assert.assertNotNull("Student name is null", studentName);
+	}
+
+//	@Test
+//	public void testTemperatureMin() {
+//		System.out.println("+++ testTemperatureMin +++");
+//		
+//		// Initialise controller
+//		WeatherController wController = WeatherController.getInstance();
+//		
+//		// Retrieve all the hours temperatures recorded as for today
+//		int nHours = wController.getTotalHours();
+//		double minTemperature = 1000;
+//		for (int i=0; i<nHours; i++) {
+//			// Hour range: 1 to nHours
+//			double temperatureVal = wController.getTemperatureForHour(i+1); 
+//			if (minTemperature > temperatureVal) {
+//				minTemperature = temperatureVal;
+//			}
+//		}
+//		
+//		// Should be equal to the min value that is cached in the controller.
+//		Assert.assertTrue(wController.getTemperatureMinFromCache() == minTemperature);
+//		
+//		// Shutdown controller
+//		wController.close();		
+//	}
+//	
+//	@Test
+//	public void testTemperatureMax() {
+//		System.out.println("+++ testTemperatureMax +++");
+//		
+//		// Initialise controller
+//		WeatherController wController = WeatherController.getInstance();
+//		
+//		// Retrieve all the hours temperatures recorded as for today
+//		int nHours = wController.getTotalHours();
+//		double maxTemperature = -1;
+//		for (int i=0; i<nHours; i++) {
+//			// Hour range: 1 to nHours
+//			double temperatureVal = wController.getTemperatureForHour(i+1); 
+//			if (maxTemperature < temperatureVal) {
+//				maxTemperature = temperatureVal;
+//			}
+//		}
+//		
+//		// Should be equal to the min value that is cached in the controller.
+//		Assert.assertTrue(wController.getTemperatureMaxFromCache() == maxTemperature);
+//		
+//		// Shutdown controller
+//		wController.close();
+//	}
+//
+//	@Test
+//	public void testTemperatureAverage() {
+//		System.out.println("+++ testTemperatureAverage +++");
+//		
+//		// Initialise controller
+//		WeatherController wController = WeatherController.getInstance();
+//		
+//		// Retrieve all the hours temperatures recorded as for today
+//		int nHours = wController.getTotalHours();
+//		double sumTemp = 0;
+//		for (int i=0; i<nHours; i++) {
+//			// Hour range: 1 to nHours
+//			double temperatureVal = wController.getTemperatureForHour(i+1); 
+//			sumTemp += temperatureVal;
+//		}
+//		double averageTemp = sumTemp / nHours;
+//		
+//		// Should be equal to the min value that is cached in the controller.
+//		Assert.assertTrue(wController.getTemperatureAverageFromCache() == averageTemp);
+//		
+//		// Shutdown controller
+//		wController.close();
+//	}
+	
+	@Test
+	public void testTemperaturePersist() {
+		System.out.println("+++ testTemperaturePersist +++");
+
+		// Initialise controller
+		WeatherController wController = WeatherController.getInstance();
+
+		// Persist temperature
+		String persistTime = wController.persistTemperature(10, 19.5);
+
+		// Format current time with only hour and minute
+		String now = new SimpleDateFormat("H:m").format(new Date());
+
+		System.out.println("Persist time: " + persistTime + ", now: " + now);
+
+		// Check if the persisted time starts with the same hour and minute
+		Assert.assertTrue(persistTime.startsWith(now));
+
+		wController.close();
+	}
+
+}
